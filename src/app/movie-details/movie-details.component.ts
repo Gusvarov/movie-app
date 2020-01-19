4afbf936664a5324edbbc22ed4b1bda0e71b5895
Gusvarov <omelchenko.nazarii@gmail.com>
2020-01-19 17:20:01 +0200

    Removed unnecessary imports

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MoviesService } from '../movie-details/movie-details.service';
 import { ActivatedRoute} from '@angular/router';
 
